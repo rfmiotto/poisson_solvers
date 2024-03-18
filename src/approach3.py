6c84a75d6c58e9554542b753de194b6a41820703
Renato Miotto <renato.fmiotto@gmail.com>
2024-03-18 01:23:38 -0300

    add: solver with conjugate gradient

--- src/approach3.py
+++ src/approach3.py
@@ -2,194 +2,132 @@
 Solve Poisson using conjugate gradient method
 """
 
-import scipy
-from scipy.sparse.csgraph import laplacian
-from scipy.ndimage import generate_binary_structure
-import numpy as np
-import numba as nb
-import matplotlib.pyplot as plt
+from typing import TypedDict
+
 import h5py
+import matplotlib.pyplot as plt
+import numba as nb
+import numpy as np
+import scipy
+from numpy.typing import NDArray
 
 f = h5py.File("../jhtdb/jhtdb_isotropic1024fine_3D_pressure.h5", "r")
 xcoor = np.array(f["xcoor"])
 
-SIZE = 256
-NUM_ITERATIONS = 10_000
+SIZE = 256 - 2
 FILENAME = "jhtdb.mat"
-SPACING = (xcoor[1] - xcoor[0]) / 2
+SPACING = xcoor[1] - xcoor[0]
 
 
+def conjgrad(operator, b, x):
+
+    r = b - operator(x)
+    p = r
+    rsold = np.dot(np.transpose(r), r)
+
+    for _ in range(len(b)):
+        Ap = operator(p)
+        alpha = rsold / np.dot(np.transpose(p), Ap)
+        x = x + np.dot(alpha, p)
+        r = r - np.dot(alpha, Ap)
+        rsnew = np.dot(np.transpose(r), r)
+        if np.sqrt(rsnew) < 1e-8:
+            break
+        p = r + (rsnew / rsold) * p
+        rsold = rsnew
+    return x
+
+
+# @nb.njit(nogil=True)
 def laplacian_operator(u_vec):
-    # apply the discrete negative Laplacian with homogeneous Dirichlet boundary conditions
-
     num_points = len(u_vec)
     n = int(np.sqrt(num_points))
     u_mat = np.reshape(u_vec, (n, n))
 
     laplacian_op = 4 * u_mat
+    laplacian_op[:-1, :] -= u_mat[1:, :]  # -u(i+1, j)
+    laplacian_op[1:, :] -= u_mat[:-1, :]  # -u(i-1, j)
+    laplacian_op[:, :-1] -= u_mat[:, 1:]  # -u(i, j+1)
+    laplacian_op[:, 1:] -= u_mat[:, :-1]  # -u(i, j-1)
 
-    laplacian_op[:-1, :] -= u_mat[1:, :]
-    laplacian_op[1:, :] -= u_mat[:-1, :]
-    laplacian_op[:, 1:] -= u_mat[:, :-1]
-    laplacian_op[:, :-1] -= u_mat[:, 1:]
-
-    h2 = (n + 1) * (n + 1)  # FIXME: I think this should be the spacing**2
-    laplacian_op *= h2
-    laplacian_op = np.reshape(laplacian_op, (num_points))  # TODO: ravel() ??
-
-    return laplacian_op
+    return laplacian_op.ravel()
 
 
-def load(source, f1, f2, f3, f4):
-    # construct load vector for system -\Delta u = f
-    n = len(f1)
-    b = source.copy()
-    h2 = (n + 1) * (n + 1)  # FIXME: This should be the spacing**2
-
-    # y=0
-    b[0, :] += f1 * h2
-
-    # x=0
-    b[:, 0] += f2 * h2
-
-    # y=length
-    b[-1, :] += f3 * h2
-
-    # x=length
-    b[:, -1] += f4 * h2
-
-    b = np.reshape(b, (n**2))  # TODO: ravel() ??
-    return b
+class BoundaryConditions(TypedDict):
+    boundary_condition1: NDArray
+    boundary_condition2: NDArray
+    boundary_condition3: NDArray
+    boundary_condition4: NDArray
 
 
-def main_web():
-    size = 100  # number of side points (I think it is the inner region, not considering the edges)
-    length = 1
-    h = length / (size + 1)  # spacing
-    num_points = size**2
+def load(source, bc: BoundaryConditions, spacing: float):
+    b = -(spacing**2) * source.copy()
 
-    x = np.linspace(h, length - h, size)
-    y = np.linspace(h, length - h, size)
-    xv = np.reshape(x, (1, size))
-    yv = np.reshape(y, (size, 1))
+    b[:, 0] += bc["boundary_condition1"]
+    b[0, :] += bc["boundary_condition2"]
+    b[:, -1] += bc["boundary_condition3"]
+    b[-1, :] += bc["boundary_condition4"]
 
-    # source
-    sig = 0.1
-    source = (
-        100
-        * np.exp((-((yv - 0.5) ** 2)) / sig**2)
-        * np.exp(-((xv - 0.5) ** 2) / sig**2)
-        / (2 * np.pi * sig)
-    )
-
-    # y=0
-    f1 = 1 * np.sin(x * np.pi)
-
-    # x=0
-    f2 = 2 * np.sin(y * np.pi)
-
-    # y=length
-    f3 = 3 * np.sin(x * np.pi)
-
-    # x=length
-    f4 = 4 * np.sin(y * np.pi)
-
-    matrix_a = scipy.sparse.linalg.LinearOperator(
-        (num_points, num_points), matvec=laplacian_operator
-    )
-    b = load(source, f1, f2, f3, f4)
-
-    u, exitcode = scipy.sparse.linalg.cg(matrix_a, b)
-
-    print(exitcode)
-
-    (xg, yg) = np.meshgrid(x, y)
-    u_mat = np.reshape(u, (size, size))
-    ax = plt.axes(projection="3d")
-    ax.plot_surface(xg, yg, u_mat)
-    plt.show()
+    return b.ravel()  # * spacing**2
 
 
 def main():
-    u = np.zeros((SIZE, SIZE))
-
-    u = set_boundary_conditions(u, FILENAME)
+    num_points_inner = SIZE**2
 
     source = get_source_term(FILENAME)
-    source *= SPACING**2
+    source_inner = source[1:-1, 1:-1]
 
-    # source = add_noise(source)
+    matrix = scipy.sparse.linalg.LinearOperator(
+        (num_points_inner, num_points_inner), matvec=laplacian_operator
+    )
 
-    operator = laplacian(kernel_operator(), form="lo")
-    linear_operator = scipy.sparse.linalg.LinearOperator((3, 3), matvec=operator)
-    u = scipy.sparse.linalg.cg(linear_operator, source)
+    true_field = scipy.io.loadmat(FILENAME)["representation"]
+    true_field_inner = true_field[1:-1, 1:-1]
+    bc: BoundaryConditions
+    bc = {
+        "boundary_condition1": true_field_inner[:, 0],
+        "boundary_condition2": true_field_inner[0, :],
+        "boundary_condition3": true_field_inner[:, -1],
+        "boundary_condition4": true_field_inner[-1, :],
+    }
 
-    # u_wrong_bc = np.zeros((SIZE, SIZE))
-    # u_wrong_bc = set_zero_dirichlet_boundary_condition(u_wrong_bc)
+    b = load(source_inner, bc, SPACING)
 
-    # with ProgressBar(total=NUM_ITERATIONS) as progress:
-    #     u_wrong_bc = iterate(NUM_ITERATIONS, u_wrong_bc, source, progress)
+    # u_inner = conjgrad(laplacian_operator, b, np.zeros((num_points_inner)))
+
+    u_inner, cg_info = scipy.sparse.linalg.cg(matrix, b, rtol="1e-12")
+
+    if cg_info != 0:
+        raise RuntimeWarning("Convergence to tolerance not achieved")
 
     ground_truth = scipy.io.loadmat(FILENAME)["representation"]
+    ground_truth_inner = ground_truth[1:-1, 1:-1]
 
-    vmax = ground_truth.max()
-    vmin = ground_truth.min()
+    vmax = ground_truth_inner.max()
+    vmin = ground_truth_inner.min()
 
     _, ax = plt.subplots(nrows=1, ncols=2)
-    ax[0].imshow(ground_truth, vmax=vmax, vmin=vmin, cmap="gray")
-    ax[1].imshow(u, vmax=vmax, vmin=vmin, cmap="gray")
+    ax[0].imshow(ground_truth_inner, vmax=vmax, vmin=vmin, cmap="gray")
+    # ax[1].imshow(u_inner.reshape((SIZE, SIZE)), vmax=vmax, vmin=vmin, cmap="gray")
+    ax[1].imshow(u_inner.reshape((SIZE, SIZE)), cmap="gray")
     # ax[2].imshow(u_wrong_bc, vmax=vmax, vmin=vmin, cmap="gray")
     ax[0].set_title("Ground truth")
     ax[1].set_title("Poisson with correct BC")
     # ax[2].set_title("Poisson with zero BC")
     plt.show()
 
-
-def kernel_operator():
-    kernel = generate_binary_structure(rank=2, connectivity=1).astype(float) / 4
-    kernel[1, 1] = 0
-    return kernel
-
-
-@nb.njit(nogil=True)
-def apply_neumann_bc(u):
-    u[:, 0] = u[:, 1]
-    u[0, :] = u[1, :]
-    u[:, -1] = u[:, -2]
-    u[-1, :] = u[-2, :]
-    return u
-
-
-def add_noise(source):
-    source_amplitude = max(source.max(), source.min())
-    noise_scale = 0.01 * source_amplitude
-    noise = noise_scale * np.random.normal(0, 1, size=SIZE**2).reshape((SIZE, SIZE))
-    source += noise
-    return source
+    # x = np.linspace(0, length, SIZE)
+    # y = np.linspace(0, length, SIZE)
+    # (xg, yg) = np.meshgrid(x, y)
+    # u_mat = np.reshape(u, (SIZE, SIZE))
+    # ax = plt.axes(projection="3d")
+    # ax.plot_surface(xg, yg, u_mat)
+    # plt.show()
 
 
 def get_source_term(file: str):
     return scipy.io.loadmat(file)["laplacian"]
 
 
-def set_boundary_conditions(u, file: str):
-    true_field = scipy.io.loadmat(file)["representation"]
-
-    u[:, 0] = true_field[:, 0]
-    u[0, :] = true_field[0, :]
-    u[:, -1] = true_field[:, -1]
-    u[-1, :] = true_field[-1, :]
-
-    return u
-
-
-def set_zero_dirichlet_boundary_condition(u):
-    u[:, 0] = 0.0
-    u[0, :] = 0.0
-    u[:, -1] = 0.0
-    u[-1, :] = 0.0
-    return u
-
-
 if __name__ == "__main__":
-    main_web()
+    main()
